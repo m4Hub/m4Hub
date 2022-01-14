eaa7364512599d6a6ec1e0656da7e0cc75db0f15
m4Hub <97708461+m4Hub@users.noreply.github.com>
2022-01-13 21:56:14 -0300

    Começando estudar programação
    vídeo aula pela Cubos Academy

---
+++ index.js
@@ -0,0 +1,32 @@
+//PRIMEIRO RUN (COMEÇANDO A ESTUDAR PROGRAMAÇÃO)
+const idade = 20;
+// COLOQUE TRUE SE A PESSOA TEM CARTEIRINHA E FALSE SE NÃO TIVER
+const temCarteirinha = true
+
+//Menor de 18 -> Menor de idade - paga meia
+//Entre 18 - 60 -> Adulto - tem que ter carteirinha para pagar meia
+//Maiores de 60 -> Idosos - paga meia
+//condicionais  - if (se) - else (se nao)
+if (idade >= 18) {
+  console.log("Pode seguir, pois é maior de idade")
+} else {
+  console.log("Barrado! Esta festa é para maiores")
+}
+
+if (idade < 18) {
+  console.log('Menor de idade');
+} else if (idade <= 60) {
+  console.log("Pessoa adulta");
+} else console.log('Pessoa idosa')
+//----------------------------------------
+if (idade < 18) {
+  console.log("meia");
+} else if (idade <=60) {
+  if (temCarteirinha === true) {
+    console.log("meia");
+  } else {
+    console.log('inteira');
+  }
+} else {
+  console.log("meia")
+}
